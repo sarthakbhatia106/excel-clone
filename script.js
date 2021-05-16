a73dd8371c01d7ef68fc10c65c39a51cef6b883e
sarthak bhatia <56428721+sarthakbhatia106@users.noreply.github.com>
2021-05-16 20:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,572 @@
+const ps = new PerfectScrollbar("#cells", {
+  wheelSpeed: 15,
+});
+
+for (let i = 1; i <= 100; i++) {
+  let str = "";
+  let n = i;
+
+  while (n > 0) {
+    let rem = n % 26;
+    if (rem == 0) {
+      str = "Z" + str;
+      n = Math.floor(n / 26) - 1;
+    } else {
+      str = String.fromCharCode(rem - 1 + 65) + str;
+      n = Math.floor(n / 26);
+    }
+  }
+  $("#columns").append(`<div class="column-name">${str}</div>`);
+  $("#rows").append(`<div class="row-name">${i}</div>`);
+}
+
+let cellData = {
+  Sheet1: {},
+};
+
+let selectedSheet = "Sheet1";
+let totalSheets = 1;
+let lastlyAddedSheet = 1;
+
+let defaultProperties = {
+  "font-family": "Noto Sans",
+  "font-size": 14,
+  text: "",
+  bold: false,
+  italic: false,
+  underlined: false,
+  alignment: "left",
+  color: "#444",
+  bgcolor: "#fff",
+};
+
+for (let i = 1; i <= 100; i++) {
+  let row = $('<div class="cell-row"></div>');
+  for (let j = 1; j <= 100; j++) {
+    row.append(
+      `<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`
+    );
+  }
+  $("#cells").append(row);
+}
+
+$("#cells").scroll(function (e) {
+  $("#columns").scrollLeft(this.scrollLeft);
+  $("#rows").scrollTop(this.scrollTop);
+});
+
+$(".input-cell").dblclick(function (e) {
+  $(".input-cell.selected").removeClass(
+    "selected top-selected bottom-selected left-selected right-selected"
+  );
+  $(this).addClass("selected");
+  $(this).attr("contenteditable", "true");
+  $(this).focus();
+});
+
+$(".input-cell").blur(function (e) {
+  $(this).attr("contenteditable", "false");
+  updateCellData("text", $(this).text());
+});
+
+function getRowCol(ele) {
+  let id = $(ele).attr("id");
+  let idArray = id.split("-");
+  let rowId = parseInt(idArray[1]);
+  let colId = parseInt(idArray[3]);
+  return [rowId, colId];
+}
+
+function getTopLeftBottomRightCell(rowId, colId) {
+  let topCell = $(`#row-${rowId - 1}-col-${colId}`);
+  let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
+  let leftCell = $(`#row-${rowId}-col-${colId - 1}`);
+  let rightCell = $(`#row-${rowId}-col-${colId + 1}`);
+  return [topCell, bottomCell, leftCell, rightCell];
+}
+$(".input-cell").click(function (e) {
+  let [rowId, colId] = getRowCol(this);
+  let [topCell, bottomCell, leftCell, rightCell] = getTopLeftBottomRightCell(
+    rowId,
+    colId
+  );
+  if ($(this).hasClass("selected") && e.ctrlKey) {
+    unselectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+  } else {
+    selectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+  }
+});
+
+function unselectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
+  if ($(ele).attr("contenteditable") == "false") {
+    if ($(ele).hasClass("top-selected")) {
+      topCell.removeClass("bottom-selected");
+    }
+
+    if ($(ele).hasClass("bottom-selected")) {
+      bottomCell.removeClass("top-selected");
+    }
+
+    if ($(ele).hasClass("left-selected")) {
+      leftCell.removeClass("right-selected");
+    }
+
+    if ($(ele).hasClass("right-selected")) {
+      rightCell.removeClass("left-selected");
+    }
+
+    $(ele).removeClass(
+      "selected top-selected bottom-selected left-selected right-selected"
+    );
+  }
+}
+
+function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
+  if (e.ctrlKey) {
+    // top selected or not
+    let topSelected;
+    if (topCell) {
+      topSelected = topCell.hasClass("selected");
+    }
+
+    // bottom selected or not
+    let bottomSelected;
+    if (bottomCell) {
+      bottomSelected = bottomCell.hasClass("selected");
+    }
+
+    // left selected or not
+    let leftSelected;
+    if (leftCell) {
+      leftSelected = leftCell.hasClass("selected");
+    }
+
+    // right selected or not
+    let rightSelected;
+    if (rightCell) {
+      rightSelected = rightCell.hasClass("selected");
+    }
+
+    if (topSelected) {
+      $(ele).addClass("top-selected");
+      topCell.addClass("bottom-selected");
+    }
+
+    if (bottomSelected) {
+      $(ele).addClass("bottom-selected");
+      bottomCell.addClass("top-selected");
+    }
+
+    if (leftSelected) {
+      $(ele).addClass("left-selected");
+      leftCell.addClass("right-selected");
+    }
+
+    if (rightSelected) {
+      $(ele).addClass("right-selected");
+      rightCell.addClass("left-selected");
+    }
+  } else {
+    $(".input-cell.selected").removeClass(
+      "selected top-selected bottom-selected left-selected right-selected"
+    );
+  }
+  $(ele).addClass("selected");
+  changeHeader(getRowCol(ele));
+}
+
+function changeHeader([rowId, colId]) {
+  console.log(cellData);
+  let data;
+  if (
+    cellData[selectedSheet][rowId - 1] &&
+    cellData[selectedSheet][rowId - 1][colId - 1]
+  ) {
+    data = cellData[selectedSheet][rowId - 1][colId - 1];
+  } else {
+    data = defaultProperties;
+  }
+  $(".alignment.selected").removeClass("selected");
+  $(`.alignment[data-type=${data.alignment}`).addClass("selected");
+  addRemoveSelectFromFontStyle(data, "bold");
+  addRemoveSelectFromFontStyle(data, "italic");
+  addRemoveSelectFromFontStyle(data, "underlined");
+  $("#fill-color").css("border-bottom", `4px solid ${data.bgcolor}`);
+  $("#text-color").css("border-bottom", `4px solid ${data.color}`);
+  $("#font-family").val(data["font-family"]);
+  $("#font-size").val(data["font-size"]);
+  $("#font-family").css("font-family", data["font-family"]);
+}
+
+function addRemoveSelectFromFontStyle(data, property) {
+  if (data[property]) {
+    $(`#${property}`).addClass("selected");
+  } else {
+    $(`#${property}`).removeClass("selected");
+  }
+}
+
+let count = 0;
+let startcellSelected = false;
+let startCell = {};
+let endCell = {};
+let scrollXRStarted = false;
+let scrollXLStarted = false;
+$(".input-cell").mousemove(function (e) {
+  e.preventDefault();
+  if (e.buttons == 1) {
+    if (e.pageX > $(window).width() - 10 && !scrollXRStarted) {
+      scrollXR();
+    } else if (e.pageX < 10 && !scrollXLStarted) {
+      scrollXL();
+    }
+    if (!startcellSelected) {
+      let [rowId, colId] = getRowCol(this);
+      startCell = { rowId: rowId, colId: colId };
+      selectAllBetweenCells(startCell, startCell);
+      startcellSelected = true;
+    }
+  } else {
+    startcellSelected = false;
+  }
+});
+
+$(".input-cell").mouseenter(function (e) {
+  if (e.buttons == 1) {
+    if (e.pageX < $(window).width() - 10 && scrollXRStarted) {
+      clearInterval(scrollXRInterval);
+      scrollXRStarted = false;
+    }
+
+    if (e.pageX > 10 && scrollXLStarted) {
+      clearInterval(scrollXLInterval);
+      scrollXLStarted = false;
+    }
+    let [rowId, colId] = getRowCol(this);
+    endCell = { rowId: rowId, colId: colId };
+    selectAllBetweenCells(startCell, endCell);
+  }
+});
+
+function selectAllBetweenCells(start, end) {
+  $(".input-cell.selected").removeClass(
+    "selected top-selected bottom-selected left-selected right-selected"
+  );
+  for (
+    let i = Math.min(start.rowId, end.rowId);
+    i <= Math.max(start.rowId, end.rowId);
+    i++
+  ) {
+    for (
+      let j = Math.min(start.colId, end.colId);
+      j <= Math.max(start.colId, end.colId);
+      j++
+    ) {
+      let [topCell, bottomCell, leftCell, rightCell] =
+        getTopLeftBottomRightCell(i, j);
+      selectCell(
+        $(`#row-${i}-col-${j}`)[0],
+        { ctrlKey: true },
+        topCell,
+        bottomCell,
+        leftCell,
+        rightCell
+      );
+    }
+  }
+}
+
+let scrollXRInterval;
+let scrollXLInterval;
+function scrollXR() {
+  scrollXRStarted = true;
+  scrollXRInterval = setInterval(() => {
+    $("#cells").scrollLeft($("#cells").scrollLeft() + 100);
+  }, 100);
+}
+
+function scrollXL() {
+  scrollXLStarted = true;
+  scrollXLInterval = setInterval(() => {
+    $("#cells").scrollLeft($("#cells").scrollLeft() - 100);
+  }, 100);
+}
+
+$(".data-container").mousemove(function (e) {
+  e.preventDefault();
+  if (e.buttons == 1) {
+    if (e.pageX > $(window).width() - 10 && !scrollXRStarted) {
+      scrollXR();
+    } else if (e.pageX < 10 && !scrollXLStarted) {
+      scrollXL();
+    }
+  }
+});
+
+$(".data-container").mouseup(function (e) {
+  clearInterval(scrollXRInterval);
+  clearInterval(scrollXLInterval);
+  scrollXRStarted = false;
+  scrollXLStarted = false;
+});
+
+$(".alignment").click(function (e) {
+  let alignment = $(this).attr("data-type");
+  $(".alignment.selected").removeClass("selected");
+  $(this).addClass("selected");
+  $(".input-cell.selected").css("text-align", alignment);
+  updateCellData("alignment", alignment);
+});
+
+$("#bold").click(function (e) {
+  setStyle(this, "bold", "font-weight", "bold");
+});
+
+$("#italic").click(function (e) {
+  setStyle(this, "italic", "font-style", "italic");
+});
+
+$("#underlined").click(function (e) {
+  setStyle(this, "underlined", "text-decoration", "underline");
+});
+
+function setStyle(ele, property, key, value) {
+  if ($(ele).hasClass("selected")) {
+    $(ele).removeClass("selected");
+    $(".input-cell.selected").css(key, "");
+    // $(".input-cell.selected").each(function (index, data) {
+    //     let [rowId, colId] = getRowCol(data);
+    //     cellData[rowId - 1][colId - 1][property] = false;
+    // });
+    updateCellData(property, false);
+  } else {
+    $(ele).addClass("selected");
+    $(".input-cell.selected").css(key, value);
+    // $(".input-cell.selected").each(function (index, data) {
+    //     let [rowId, colId] = getRowCol(data);
+    //     cellData[rowId - 1][colId - 1][property] = true;
+    // });
+    updateCellData(property, true);
+  }
+}
+
+$(".pick-color").colorPick({
+  initialColor: "#abcd",
+  allowRecent: true,
+  recentMax: 5,
+  allowCustomColor: true,
+  palette: [
+    "#1abc9c",
+    "#16a085",
+    "#2ecc71",
+    "#27ae60",
+    "#3498db",
+    "#2980b9",
+    "#9b59b6",
+    "#8e44ad",
+    "#34495e",
+    "#2c3e50",
+    "#f1c40f",
+    "#f39c12",
+    "#e67e22",
+    "#d35400",
+    "#e74c3c",
+    "#c0392b",
+    "#ecf0f1",
+    "#bdc3c7",
+    "#95a5a6",
+    "#7f8c8d",
+  ],
+  onColorSelected: function () {
+    if (this.color != "#ABCD") {
+      if ($(this.element.children()[1]).attr("id") == "fill-color") {
+        $(".input-cell.selected").css("background-color", this.color);
+        $("#fill-color").css("border-bottom", `4px solid ${this.color}`);
+        // $(".input-cell.selected").each((index, data) => {
+        //     let [rowId, colId] = getRowCol(data);
+        //     cellData[rowId - 1][colId - 1].bgcolor = this.color;
+        // });
+        updateCellData("bgcolor", this.color);
+      }
+      if ($(this.element.children()[1]).attr("id") == "text-color") {
+        $(".input-cell.selected").css("color", this.color);
+        $("#text-color").css("border-bottom", `4px solid ${this.color}`);
+        // $(".input-cell.selected").each((index, data) => {
+        //     let [rowId, colId] = getRowCol(data);
+        //     cellData[rowId - 1][colId - 1].color = this.color;
+        // });
+        updateCellData("color", this.color);
+      }
+    }
+  },
+});
+
+$("#fill-color").click(function (e) {
+  setTimeout(() => {
+    $(this).parent().click();
+  }, 10);
+});
+
+$("#text-color").click(function (e) {
+  setTimeout(() => {
+    $(this).parent().click();
+  }, 10);
+});
+
+$(".menu-selector").change(function (e) {
+  let value = $(this).val();
+  let key = $(this).attr("id");
+  if (key == "font-family") {
+    $("#font-family").css(key, value);
+  }
+  if (!isNaN(value)) {
+    value = parseInt(value);
+  }
+
+  $(".input-cell.selected").css(key, value);
+  // $(".input-cell.selected").each((index, data) => {
+  //     let [rowId, colId] = getRowCol(data);
+  //     cellData[rowId - 1][colId - 1][key] =function changeHeader([rowId, colId]) {
+ value;
+  // })
+  updateCellData(key, value);
+});
+
+function updateCellData(property, value) {
+  if (value != defaultProperties[property]) {
+    $(".input-cell.selected").each(function (index, data) {
+      let [rowId, colId] = getRowCol(data);
+      if (cellData[selectedSheet][rowId - 1] == undefined) {
+        cellData[selectedSheet][rowId - 1] = {};
+        cellData[selectedSheet][rowId - 1][colId - 1] = {
+          ...defaultProperties,
+        };
+        cellData[selectedSheet][rowId - 1][colId - 1][property] = value;
+      } else {
+        if (cellData[selectedSheet][rowId - 1][colId - 1] == undefined) {
+          cellData[selectedSheet][rowId - 1][colId - 1] = {
+            ...defaultProperties,
+          };
+          cellData[selectedSheet][rowId - 1][colId - 1][property] = value;
+        } else {
+          cellData[selectedSheet][rowId - 1][colId - 1][property] = value;
+        }
+      }
+    });
+  } else {
+    $(".input-cell.selected").each(function (index, data) {
+      let [rowId, colId] = getRowCol(data);
+      if (
+        cellData[selectedSheet][rowId - 1] &&
+        cellData[selectedSheet][rowId - 1][colId - 1]
+      ) {
+        cellData[selectedSheet][rowId - 1][colId - 1][property] = value;
+        if (
+          JSON.stringify(cellData[selectedSheet][rowId - 1][colId - 1]) ==
+          JSON.stringify(defaultProperties)
+        ) {
+          delete cellData[selectedSheet][rowId - 1][colId - 1];
+          if (Object.keys(cellData[selectedSheet][rowId - 1]).length == 0) {
+            delete cellData[selectedSheet][rowId - 1];
+          }
+        }
+      }
+    });
+  }
+}
+$(".container").click(function(e){
+  $(".sheet-options-modal").remove();
+})
+
+function addSheetEvents(){
+  $(".sheet-tab.selected").on("contextmenu", function (e) {
+    e.preventDefault();
+    
+    $(".sheet-tab.selected").removeClass("selected");
+    $(this).addClass("selected");
+    selectSheet();
+
+    $(".sheet-options-modal").remove();
+    let modal = $(`<div class="sheet-options-modal">
+                  <div class="option sheet-rename">Rename</div>
+                  <div class="option sheet-delete">Delete</div>
+                </div>`);
+    modal.css({ left: e.pageX });
+    $(".container").append(modal);
+
+  });
+
+  $(".sheet-tab.selected").click(function (e) {
+    $(".sheet-tab.selected").removeClass("selected");
+    $(this).addClass("selected");
+    selectSheet();
+  });
+
+}
+addSheetEvents();
+
+$(".add-sheet").click(function (e) {
+  lastlyAddedSheet++;
+  totalSheets++;
+  cellData[`Sheet${lastlyAddedSheet}`]={};
+  $(".sheet-tab.selected").removeClass("selected");
+  $(".sheet-tab-container").append(`<div class="sheet-tab selected">Sheet${lastlyAddedSheet}</div>`);
+  selectSheet();
+  addSheetEvents();
+});
+
+
+function selectSheet(){
+  emptyPreviousSheet();
+  selectedSheet=$(".sheet-tab.selected").text();
+  loadCurrentSheet();
+  $("#row-1-col-1").click();
+}
+
+function emptyPreviousSheet() {
+  let data = cellData[selectedSheet];
+  let rowKeys = Object.keys(data);
+  for (let i of rowKeys) {
+    let rowId = parseInt(i);
+    let colKeys = Object.keys(data[rowId]);
+    for (let j of colKeys) {
+      let colId = parseInt(j);
+      let cell = $(`#row-${rowId + 1}-col-${colId + 1}`);
+      cell.text("");
+      cell.css({
+        "font-family": "NotoSans",
+        "font-size": 14,
+        "background-color": "#fff",
+        color: "#444",
+        "font-weight": "",
+        "font-style": "",
+        "text-decoration": "",
+        "text-align": "left",
+      });
+    }
+  }
+}
+
+function loadCurrentSheet() {
+  let data = cellData[selectedSheet];
+  let rowKeys = Object.keys(data);
+  for (let i of rowKeys) {
+    let rowId = parseInt(i);
+    let colKeys = Object.keys(data[rowId]);
+    for (let j of colKeys) {
+      let colId = parseInt(j);
+      let cell = $(`#row-${rowId + 1}-col-${colId + 1}`);
+      cell.text(data[rowId][colId].text);
+      cell.css({
+        "font-family": data[rowId][colId]["font-family"],
+        "font-size": data[rowId][colId]["font-size"],
+        "background-color": data[rowId][colId]["bgcolor"],
+        color: data[rowId][colId].color,
+        "font-weight": data[rowId][colId].bold ? "bold" : "",
+        "font-style": data[rowId][colId].italic ? "italic" : "",
+        "text-decoration": data[rowId][colId].underlined ? "underlined" : "",
+        "text-align": data[rowId][colId].alignment,
+      });
+    }
+  }
+}
